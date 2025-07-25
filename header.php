948ea7b47f10235bf9a975ac644647cb355acdfe
ghgamble <grace.h.gamble@gmail.com>
2025-07-25 10:43:45 -0600

    add google tag

--- header.php
+++ header.php
@@ -32,6 +32,16 @@
       "priceRange": "$$"
     }
   </script>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17383327544">
+  </script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'AW-17383327544');
+  </script>
 </head>
 <body <?php body_class(); ?>>
 
